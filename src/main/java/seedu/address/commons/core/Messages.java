89ee09f99c31cd27dd62dd8dac4f1109eda4a123
ZENN\User <miljy123@gmail.com>
2023-10-01 17:31:50 +0800

    Implement Remark Command

---
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,9 @@
+package seedu.address.commons.core;
+
+/**
+ * Contains messages common to multiple commands
+ */
+public class Messages {
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "Invalid person index.";
+}
